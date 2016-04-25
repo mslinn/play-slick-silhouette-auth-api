9842b01221ace3ac613fb40c8a7631fce81c0d91
Marek Kadek <kadek.marek@gmail.com>
2016-04-25 15:45:39 +1200

    more inejct remove

--- auth-api/app/service/impl/UserTokenService.scala
+++ auth-api/app/service/impl/UserTokenService.scala
@@ -3,10 +3,9 @@ package service.impl
 import java.time.LocalDateTime
 import java.util.UUID
 
-import com.google.inject.Inject
 import model.core.UserToken
 import model.core.UserToken.UserTokenAction
-import service.{Hasher, UserTokenService}
+import service.{ Hasher, UserTokenService }
 
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.Future
@@ -21,7 +20,7 @@ class UserTokenServiceImpl extends UserTokenService {
 /**
   * Not thread safe. Usage in production is discouraged (as it needs to be singleton).
   */
-class InMemoryUserTokenServiceImpl @Inject() (hasher: Hasher) extends UserTokenService {
+class InMemoryUserTokenServiceImpl(hasher: Hasher) extends UserTokenService {
 
   val tokens: ArrayBuffer[UserToken] = ArrayBuffer.empty[UserToken]
 
