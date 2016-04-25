5c94f671bc2081ece0d6e19448d863b85359c0f5
Marek Kadek <kadek.marek@gmail.com>
2016-04-26 09:07:21 +1200

    timestamp instaed of localdatetime

--- auth-api/app/persistence/model/TablesDefinitions.scala
+++ auth-api/app/persistence/model/TablesDefinitions.scala
@@ -14,11 +14,19 @@ trait TablesDefinitions extends ModelMappingSupport with HasAuthDbProfile {
   sealed class UserTokenMapping(tag: Tag) extends Table[UserToken](tag, "usertokens") {
     def token: Rep[String] = column[String]("token", O.PrimaryKey)
     def userUuid: Rep[String] = column[String]("useruuid")
-    def expiresOn: Rep[LocalDateTime] = column[LocalDateTime]("expireson")
+    // TODO: Localdatetime fails on postgres
+    //def expiresOn: Rep[LocalDateTime] = column[LocalDateTime]("expireson")
+    def expiresOn: Rep[Timestamp] = column[Timestamp]("expireson")
     def tokenAction: Rep[UserTokenAction] = column[UserTokenAction]("tokenaction")
 
+    def from(x: UserToken): Option[(String, String, Timestamp, UserTokenAction)] = Some {
+      (x.token, x.userUuid, Timestamp.valueOf(x.expiresOn), x.tokenAction)
+    }
+
+    def to(x:(String, String, Timestamp, UserTokenAction)): UserToken = UserToken(x._1, x._2, x._3.toLocalDateTime, x._4)
+
     override def * : ProvenShape[UserToken] =
-      (token, userUuid, expiresOn, tokenAction) <> ((UserToken.apply _).tupled, UserToken.unapply)
+      (token, userUuid, expiresOn, tokenAction) <> (to,from)
   }
 
   val userTokensQuery = TableQuery[UserTokenMapping]
