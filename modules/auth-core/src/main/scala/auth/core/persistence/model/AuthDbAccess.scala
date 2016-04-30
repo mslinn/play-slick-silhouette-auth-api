9e95e85e1c6ddf8c40107a0e8d2fa72d6db9f96e
Marek Kadek <kadek.marek@gmail.com>
2016-04-30 20:58:18 +1200

    init

--- modules/auth-core/src/main/scala/auth/core/persistence/model/AuthDbAccess.scala
+++ modules/auth-core/src/main/scala/auth/core/persistence/model/AuthDbAccess.scala
@@ -2,6 +2,8 @@ package auth.core.persistence.model
 
 import auth.core.persistence.AuthDbProfile
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.backend.DatabaseConfig
+import slick.profile.BasicProfile
 
 /**
   * Mix in this driver if you are using dependency injection and require
@@ -26,3 +28,21 @@ trait AuthDbAccess extends HasDatabaseConfigProvider[AuthDbProfile]
   * You can use it to get multiple different DBs in single instance
   */
 trait AuthDatabaseConfigProvider extends DatabaseConfigProvider
+
+trait AuthDatabaseDriver extends HasDbDriver[AuthDbProfile]
+
+trait AuthDatabaseDriverConfigProvider extends HasDbDriverConfigProvider[AuthDbProfile]
+
+trait HasDbDriver[P <: BasicProfile] {
+  /** The Slick database configuration. */
+  protected val dbConfig: DatabaseConfig[P] // field is declared as a val because we want a stable identifier.
+  /** The Slick driver extracted from `dbConfig`. */
+  protected final lazy val driver: P = dbConfig.driver // field is lazy to avoid early initializer problems.
+}
+
+trait HasDbDriverConfigProvider[P <: BasicProfile] extends HasDbDriver[P] {
+  /** The provider of a Slick `DatabaseConfig` instance.*/
+  protected val dbConfigProvider: DatabaseConfigProvider
+  override final protected val dbConfig: DatabaseConfig[P] = dbConfigProvider.get[P]
+}
+
